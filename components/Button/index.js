7af3b0e6ab58c7d744b20198d388aea2d5300878
Filipe <medeirolp@gmail.com>
2022-10-14 20:15:12 -0300

    melhorando posicionamento do elementos na tela

--- components/Button/index.js
+++ components/Button/index.js
@@ -3,10 +3,12 @@ import Link from 'next/link';
 
 export default function Button({ textButton, navigation }) {
     return (
+
         <Link href={`${navigation}`} passHref>
             <button className={styles.button}>
                 {textButton}
             </button>
         </Link >
+
     )
 }
--- pages/portifolio/index.js
+++ pages/portifolio/index.js
@@ -8,41 +8,40 @@ import ContentPortifolio from '../../components/ContentPortifolio';
 export default function Portifolio() {
 
     return (
-        <div className={styles.container}>
-            <ContentPortifolio
-                image={FioAFioImage}
-                title='Fio a Fio'
-                description='Fio a fio clássico ou fio a fio efeito back é uma técnica que consiste em aplicar um fio sintético em cada fio natural, alongando os cílios e trazendo um leve volume deixandos cilios além de alongados mais curvados mantendo a naturalidade do olhar.'
-                textButton='Entre em contato'
-                navigation='contato'
-            />
-            <ContentPortifolio
-                image={VolumeBrasileiroImage}
-                title='Volume Brasileiro'
-                description='O Volume brasileiro é o queridinho do momento, isso porque além de alongar ele também dá volume aos cílios sem perder a delicadeza no olhar.'
-                textButton='Entre em contato'
-                navigation='contato'
-            />
+        <>
 
-            <ContentPortifolio
-                image={LashLifting}
-                title='Lash Lifting'
-                description='
-                O lash lifting cuida dos cílios para que eles fiquem com aspecto natural, de forma que é possível curvá-los,  e hidratá-los profundamente. A ideia é ter os cílios perfeitos sem precisar dar manutenção ou passar curvex.
-                Diferente da extensão de cílios no lash lifting não são usados fios artificiais e também não precisa de manutenção.
-                O procedimento leva em média 45 min é completamente indolor  e seu efeito tem duração de 4 a 8 semanas dependendo dos cuidados de cada pessoa.'
-                textButton='Entre em contato'
-                navigation='contato'
-            />
-
-
-            <ContentPortifolio
-                image={Design}
-                title='Design de sobrancelhas'
-                description='Antigamente era usado um padrão para as sobrancelhas, com isso o formato único de cada sobrancelha não era respeitado, hoje em dia o foco do design é fugir do “molde pronto” e, proporcionar sobrancelhas perfeitas respeitando as diferenças de cada rosto.'
-                textButton='Entre em contato'
-                navigation='contato'
-            />
-        </div >
+            <div className={styles.container}>
+                <ContentPortifolio
+                    image={FioAFioImage}
+                    title='Fio a Fio'
+                    description='Fio a fio clássico ou fio a fio efeito back é uma técnica que consiste em aplicar um fio sintético em cada fio natural, alongando os cílios e trazendo um leve volume deixandos cilios além de alongados mais curvados mantendo a naturalidade do olhar.'
+                    textButton='Entre em contato'
+                    navigation='contato'
+                />
+                <ContentPortifolio
+                    image={VolumeBrasileiroImage}
+                    title='Volume Brasileiro'
+                    description='O Volume brasileiro é o queridinho do momento, isso porque além de alongar ele também dá volume aos cílios sem perder a delicadeza no olhar.'
+                    textButton='Entre em contato'
+                    navigation='contato'
+                />
+                <ContentPortifolio
+                    image={LashLifting}
+                    title='Lash Lifting'
+                    description=' O lash lifting cuida dos cílios para que eles fiquem com aspecto natural, de forma que é possível curvá-los,  e hidratá-los profundamente. A ideia é ter os cílios perfeitos sem precisar dar manutenção ou passar curvex.
+                    Diferente da extensão de cílios no lash lifting não são usados fios artificiais e também não precisa de manutenção.
+                    O procedimento leva em média 45 min é completamente indolor  e seu efeito tem duração de 4 a 8 semanas dependendo dos cuidados de cada pessoa.'
+                    textButton='Entre em contato'
+                    navigation='contato'
+                />
+                <ContentPortifolio
+                    image={Design}
+                    title='Design de sobrancelhas'
+                    description='Antigamente era usado um padrão para as sobrancelhas, com isso o formato único de cada sobrancelha não era respeitado, hoje em dia o foco do design é fugir do “molde pronto” e, proporcionar sobrancelhas perfeitas respeitando as diferenças de cada rosto.'
+                    textButton='Entre em contato'
+                    navigation='contato'
+                />
+            </div>
+        </>
     )
 }
